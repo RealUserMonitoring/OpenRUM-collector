3d21b7dce58e842f268a099b7ad3048a1eeefe41
zhaochunhui <zhaochunhui@bonree.com>
2023-01-04 11:03:23 +0800

    modify nacos configuration

--- src/main/java/com/openrum/collector/StartApplication.java
+++ src/main/java/com/openrum/collector/StartApplication.java
@@ -3,15 +3,15 @@ package com.openrum.collector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class StartApplication {
 
     public static void main(String[] args) {
-        SpringApplication
-                .run(StartApplication.class, args);
+        SpringApplication.run(StartApplication.class, args);
     }
 
 }
